82c81a8c68682441bbef40bbbfd1b6e6b3a8280a
Your Name <you@example.com>
2026-04-09 05:55:17 +0000

    sd

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,11 +1,4 @@
-/**
- * Represents a classroom.
- */
 export default class ClassRoom {
-    /**
-     * Creates a new ClassRoom.
-     * @param {Number} maxStudentsSize - The maximum number of students in the class.
-     */
     constructor(maxStudentsSize) {
       this._maxStudentsSize = maxStudentsSize;
     }
